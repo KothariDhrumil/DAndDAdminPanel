6c77c0e94960617a96d43677de912d3cb09a494f
Dhrumil Kothari <kotharidhrumil.ite@live.in>
2025-10-05 18:46:36 +0530

    Update token Service and support ticket domain

--- Application/Identity/Tokens/ITokenService.cs
+++ Application/Identity/Tokens/ITokenService.cs
@@ -8,10 +8,10 @@ public interface ITokenService : ITransientService
 {
     Task<Result> AuthenticateAsync(TokenRequest request, CancellationToken cancellationToken);
 
-    Task<Result<TokenResponse>> RefreshTokenAsync(RefreshTokenRequest request);
+    Task<Result> RefreshTokenAsync(RefreshTokenRequest request);
 
-    Task<Result<TokenResponse>> ConfirmPhoneAsync(string phoneNumber, string code, string ipAddress);
+    Task<Result> ConfirmPhoneAsync(string phoneNumber, string code, string ipAddress);
 
-    Task<Result<string>> GenerateOTPAsync(GenerateOTPRequest request);
+    Task<Result> GenerateOTPAsync(GenerateOTPRequest request);
 }
 
