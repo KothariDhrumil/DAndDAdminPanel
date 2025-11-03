2727d61b57474da47e17b3a44a36ed68d7e858d6
Dhrumil Kothari <kotharidhrumil.ite@live.in>
2025-11-03 15:12:11 +0530

    Minor changes in Purchase

--- Domain/Purchase/Purchase.cs
+++ Domain/Purchase/Purchase.cs
@@ -6,11 +6,11 @@ namespace Domain.Purchase
     public class Purchase : AuditableEntity
     {
         public int? RouteId { get; set; }
-        public Route Route { get; set; } = default!;
+        public Route? Route { get; set; } = default!;
 
         // PurchaseUnit can represent a warehouse OR a manufacturing unit
         public int PurchaseUnitId { get; set; }
-        public PurchaseUnit PurchaseUnit { get; set; } = default!;
+        public PurchaseUnit PurchaseUnit { get; set; } = null!;
 
         public DateTime PurchaseDate { get; set; }
         public DateTime? OrderPickupDate { get; set; }
@@ -26,7 +26,15 @@ namespace Domain.Purchase
 
         public Guid? PickupSalesmanId { get; set; }
         public TenantUserProfile? PickupSalesman { get; set; }
+        public PurchaseType Type { get; set; }
 
         public ICollection<PurchaseDetail> PurchaseDetails { get; set; } = new List<PurchaseDetail>();
+    }
+
+    public enum PurchaseType
+    {
+        Vendor = 1,
+        Route = 2,
+        InternalTransfer = 3
     }
 }
